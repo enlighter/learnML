b140fd580049a699b26fd2784fd8fc7cdebff1ab
enlighter <mandal.sushovan92@gmail.com>
2015-08-23 16:39:08 +0530

    implement naive bayes along with accuracy estimation

--- naivebayes/ClassifyNB.py
+++ naivebayes/ClassifyNB.py
@@ -1,14 +1,20 @@
 from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
-def classify(features_train, labels_train):   
+def classify(features_train, labels_train, features_test, labels_test):   
     ### import the sklearn module for GaussianNB
     ### create classifier
     ### fit the classifier on the training features and labels
     ### return the fit classifier
     
-        
-    ### your code goes here!
+    
     
     clf = GaussianNB()
     clf.fit(features_train, labels_train)
-    return clf
+    prediction = clf.predict(features_test)
+    accuracy = accuracy_score(prediction, labels_test)
+
+    #alternatively,
+    #accuracy = clf.score(features_test, labels_test)
+
+    return clf, accuracy
--- naivebayes/studentMain.py
+++ naivebayes/studentMain.py
@@ -11,7 +11,9 @@
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture, output_image
 from ClassifyNB import classify
+#from classify import NBAccuracy
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pylab as pl
 
@@ -27,15 +29,10 @@ grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if l
 bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
 
 
-
-clf = classify(features_train, labels_train)
-
+clf, accuracy = classify(features_train, labels_train, features_test, labels_test)
 
 
-    ### draw the decision boundary with the text points overlaid
+### draw the decision boundary with the text points overlaid
 prettyPicture(clf, features_test, labels_test)
 output_image("test.png", "png", open("test.png", "rb").read())
-
-
-
-
+print "Classification accuracy = ", accuracy
