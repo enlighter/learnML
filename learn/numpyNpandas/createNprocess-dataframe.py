58659022ca41f7e793cc1920e1c8275167974685
enlighter <mandal.sushovan92@gmail.com>
2016-04-20 07:36:47 +0530

    add dataframe processing

--- learn/numpyNpandas/createNprocess-dataframe.py
+++ learn/numpyNpandas/createNprocess-dataframe.py
@@ -1,4 +1,5 @@
 from pandas import DataFrame, Series
+from numpy import mean
 
 #################
 # Syntax Reminder:
@@ -38,12 +39,41 @@ def create_dataframe():
     bronze = [9, 10, 5, 12, 9, 5, 2, 1, 5, 7, 1, 2, 2, 6, 2, 4, 3, 1, 2, 1, 0, 6, 2, 1, 0, 1]
 
     olympic_medal_counts_df = DataFrame({
-            'Country' : Series(countries),
-            'Gold' : Series(gold),
-            'Silver' : Series(silver),
-            'Bronze' : Series(bronze),
+            'country' : Series(countries),
+            'gold' : Series(gold),
+            'silver' : Series(silver),
+            'bronze' : Series(bronze),
         })
 
     return olympic_medal_counts_df
 
+def avg_medal_count():
+    '''
+    Compute the average number of bronze medals earned by countries who 
+    earned at least one gold medal.  
+    
+    Save this to a variable named avg_bronze_at_least_one_gold. You do not
+    need to call the function in your code when running it in the browser -
+    the grader will do that automatically when you submit or test it.
+    
+    HINT-1:
+    You can retrieve all of the values of a Pandas column from a 
+    data frame, "df", as follows:
+    df['column_name']
+    
+    HINT-2:
+    The numpy.mean function can accept as an argument a single
+    Pandas column. 
+    
+    For example, numpy.mean(df["col_name"]) would return the 
+    mean of the values located in "col_name" of a dataframe df.
+    '''
+
+    df = create_dataframe()
+    
+    avg_bronze_at_least_one_gold =  mean(df.bronze[df.gold >= 1])
+
+    return avg_bronze_at_least_one_gold
+
 print create_dataframe()
+print avg_medal_count()
